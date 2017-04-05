7d210f6dc3828974146c68cb51039495e92670b1
Allen Canning <allen_canning@putnam.com>
2017-04-05 11:01:39 -0400

    Added test to see if subscription-manager is installed

--- lib/facter/rhnkey.rb
+++ lib/facter/rhnkey.rb
@@ -6,9 +6,13 @@ Facter.add (:rhnkey) do
   # used to use 'puppet resource user | findstr /i admin21ms'
   confine :kernel => :linux
   setcode do
-    key = Facter::Core::Execution.execute('/sbin/subscription-manager list --consumed --matches="Red Hat Enterprise Linux Server" | grep  "^Serial:" | awk \'{ print $2 }\'')
-    if key.nil? || key.empty?
-      key=false 
+    if File.file?('/sbin/subscription-manager')
+      key = Facter::Core::Execution.execute('/sbin/subscription-manager list --consumed --matches="Red Hat Enterprise Linux Server" | grep  "^Serial:" | awk \'{ print $2 }\'')
+      if key.nil? || key.empty?
+        key=false 
+      end
+    else
+      key=false
     end
     key
   end
